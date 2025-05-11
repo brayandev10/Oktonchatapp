08fd62fa695c1577795af83d74c3c0f782786696
Brayan dev <143706179+brayandev10@users.noreply.github.com>
2025-05-11 18:35:10 +0100

    Revert "First commit (#1)"
    This reverts commit cf90ae5b328da3677e96ab04ee5144e3071f069e.

--- djangochat/urls.py
+++ djangochat/urls.py
@@ -2,20 +2,11 @@ from django.contrib import admin
 from django.urls import path
 from chat import views
 
-from django.conf import settings
-from django.conf.urls.static import static
-
-
 urlpatterns = [
-   path('admin/', admin.site.urls),
-   path('', views.index, name='index'),
-    path('home/', views.home, name='home'),
-    path('login/', views.login_view, name='login'),
-    path('register/', views.register_view, name='register'),
-    path('logout/', views.logout_view, name='logout'),
-    path('checkview', views.checkview, name='checkview'),
-    path('send/', views.send, name='send'),
-    path('getMessages/<str:room>/', views.getMessages, name='getMessages'),
-    path('<str:room>/', views.room, name='room'),
-   
-] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    path('admin/', admin.site.urls),
+    path('', views.home , name ="home"),
+    path('<str:room>/', views.room , name ="room"),
+    path('checkview', views.checkview , name ="checkview"),
+    path('send', views.send , name ="send"),
+    path('getMessages/<str:room>/', views.getMessages , name ="getMessages")
+]
